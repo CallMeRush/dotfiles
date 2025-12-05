e21f50306e5415bbed14014f81dc0b28157aa700
a84426610 <root@A84426610-HGkHp.china.huawei.com>
2025-12-05 16:47:11 +0100

    [NVIM] Print without carriage \r

--- nvim/lua/config/keybindings.lua
+++ nvim/lua/config/keybindings.lua
@@ -1,3 +1,22 @@
+--- General
+-- Print without carriage
+vim.keymap.set('n', 'p', function()
+  local reg = vim.fn.getreg('+')
+  if type(reg) == "string" then
+    reg = reg:gsub('\r', '')
+    vim.fn.setreg('+', reg)
+  end
+  vim.cmd('normal! p')
+end, { silent = true, desc = "Paste without ^M" })
+vim.keymap.set('n', 'P', function()
+  local reg = vim.fn.getreg('+')
+  if type(reg) == "string" then
+    reg = reg:gsub('\r', '')
+    vim.fn.setreg('+', reg)
+  end
+  vim.cmd('normal! P')
+end, { silent = true, desc = "Paste before without ^M" })
+
 --- nvim-tree
 -- Focus folder tree
 vim.keymap.set('n', '<C-o>', ':NvimTreeFocus<CR>', { noremap = true, silent = true })

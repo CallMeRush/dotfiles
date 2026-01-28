f75db41cdc626c9a578c636421722fdf8833bab2
a84426610 <root@A84426610-HGkHp.china.huawei.com>
2026-01-28 16:31:24 +0100

    [GENERAL] Add fresh-install script

---
+++ fresh-install.sh
@@ -0,0 +1,16 @@
+cd ~
+
+# Install Oh-My-Zsh
+if [ ! -d "~/.oh-my-zsh" ]; then
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+fi
+
+# Symlink for zsh
+rm .zshrc
+ln -s dotfiles/.zshrc .zshrc
+
+# Symlink for .config
+mkdir -p .config
+ln -s dotfiles/nvim .config/nvim
+ln -s dotfiles/tmux .config/tmux
+

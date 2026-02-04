95428f765a966016de0053d9e153780b3e627839
a84426610 <root@A84426610-HGkHp.china.huawei.com>
2026-02-04 13:17:42 +0100

    [TMUX] Change settings for tmux color

--- tmux/detect-ssh.sh
+++ tmux/detect-ssh.sh
@@ -3,11 +3,14 @@ PANE_ID=$(tmux list-panes -t "$WIN_ID" -F "#{pane_id}")
 CMD_IN_PANE=$(tmux display-message -t "$pane" -p "#{pane_current_command}")
 
 case "$CMD_IN_PANE" in
-    ssh|ssh*) 
+    bz|bz*) 
         tmux set -g status-style "bg=blue,fg=black"
-        break ;;
-    *)
+        ;;
+    gz|gz*) 
         tmux set -g status-style "bg=green,fg=black"
-        break ;;
+        ;;
+    *)
+        tmux set -g status-style "bg=colour13,fg=black"
+        ;;
 esac
 
